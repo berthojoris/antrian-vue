a9a48a7fa946624c89d4c9d75c7ac757e88c911f
Bertho <bertho@pawoon.com>
2019-03-02 15:34:53 +0700

    Create model, controller, migration, factory

---
+++ database/migrations/2019_03_02_082254_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('table_id');
+            $table->string('status')->default('CREATED');
+            $table->string('user_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
