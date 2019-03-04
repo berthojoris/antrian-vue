382cd5a8fd05675c6fcc331f59c7c8c869418b77
Bertho <bertho@pawoon.com>
2019-03-04 14:31:31 +0700

    Add scheduler for truncate socket table

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call(function () {
+            DB::table('websockets_statistics_entries')->truncate();
+        })->everyMinute();
     }
 
     /**
